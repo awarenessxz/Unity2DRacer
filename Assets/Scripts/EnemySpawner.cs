bffc44291e270849119a4ca831b2a621f302b310
awarenessxz <bryankoh137@hotmail.com>
2018-12-11 20:18:59 +0800

    Completed Tutorial Online

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject[] enemies;
+    public float boundaryLeft = -1.9f;
+    public float boundaryRight = 2f;
+    public float delayTimer;
+    float timer;
+
+    // Use this for initialization
+    void Start () {
+        timer = delayTimer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Vector3 carPos = new Vector3(Random.Range(boundaryLeft, boundaryRight), transform.position.y, transform.position.z);
+            int index = Random.Range(0, enemies.Length);
+            Instantiate(enemies[index], carPos, transform.rotation);
+            timer = delayTimer;
+        }
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+    public Text scoreText;
+    public Button[] buttons;
+    int score;
+    bool gameOver;
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        gameOver = false;
+        InvokeRepeating("ScoreUpdate", 1f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoreText.text = "Score : " + score;
+	}
+
+    // Method to update Score
+    void ScoreUpdate()
+    {
+        if (!gameOver)
+        {
+            score += 1;
+        }
+    }
+
+    public void GameEnded()
+    {
+        gameOver = true;
+        TogglePause();
+        foreach(Button button in buttons)
+        {
+            button.gameObject.SetActive(true);
+        }
+    }
+
+    // replay Game
+    public void Replay()
+    {
+        TogglePause();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    // Exit Game
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    // Pause Game
+    public void TogglePause()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+        }
+        else if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
