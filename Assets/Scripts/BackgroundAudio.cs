ff0c599af22ea6bddb52d888306c6a29b4e93398
awarenessxz <bryankoh137@hotmail.com>
2018-12-12 17:20:00 +0800

    Added Background Music

---
+++ Assets/Scripts/BackgroundAudio.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundAudio : MonoBehaviour {
+
+    public AudioSource music;
+
+    // Use this for initialization
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("BgMusic");
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void StopMusic()
+    {
+        DestroyImmediate(this.gameObject);
+    }
+}
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -10,11 +10,12 @@ public class MenuManager : MonoBehaviour {
     public string raceMode = "RacerMode";
     public string challengeMode = "ChallengeMode";
     public string destroyMode = "DestroyMode";
+    private BackgroundAudio bg;
 
     // Use this for initialization
     void Start () {
-		
-	}
+        bg = GameObject.FindGameObjectWithTag("BgMusic").GetComponent<BackgroundAudio>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -23,16 +24,19 @@ public class MenuManager : MonoBehaviour {
 
     public void LoadRaceMode()
     {
+        bg.StopMusic();
         SceneManager.LoadScene(raceMode);
     }
 
     public void LoadChallengeMode()
     {
+        bg.StopMusic();
         SceneManager.LoadScene(challengeMode);
     }
 
     public void LoadDestoryMode()
     {
+        bg.StopMusic();
         SceneManager.LoadScene(destroyMode);
     }
 
